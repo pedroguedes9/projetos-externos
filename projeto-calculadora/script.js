282c35bac77e8517c7cab307e1177e7fb7c50dfc
pedroguedes9 <160842810+pedroguedes9@users.noreply.github.com>
2024-07-30 18:24:58 -0300

    calculadora

---
+++ projeto-calculadora/script.js
@@ -0,0 +1,36 @@
+function number(n1) {
+    return n1
+}
+
+document.querySelector('#digitos1 > #number1').addEventListener('click', function() {
+    number(1)
+    console.log(number(1))
+})
+document.querySelector('#digitos1 > #number2').addEventListener('click', function() {
+    number(2)
+})
+document.querySelector('#digitos1 > #number3').addEventListener('click', function() {
+    number(3)
+})
+document.querySelector('#digitos1 > #number4').addEventListener('click', function() {
+    number(4)
+})
+document.querySelector('#digitos1 > #number5').addEventListener('click', function() {
+    number(5)
+})
+document.querySelector('#digitos1 > #number6').addEventListener('click', function() {
+    number(6)
+})
+document.querySelector('#digitos1 > #number7').addEventListener('click', function() {
+    number(7)
+})
+document.querySelector('#digitos1 > #number8').addEventListener('click', function() {
+    number(8)
+})
+document.querySelector('#digitos1 > #number9').addEventListener('click', function() {    
+    number(9)
+})
+document.querySelector('#digitos1 > #number0').addEventListener('click', function() {
+    number(0)
+})
+
