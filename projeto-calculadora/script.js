aaeef40f30798cac6771b10acf77b8078145315b
pedroguedes9 <160842810+pedroguedes9@users.noreply.github.com>
2024-07-30 21:07:45 -0300

    quase terminei a calculadora

--- projeto-calculadora/script.js
+++ projeto-calculadora/script.js
@@ -1,36 +1,118 @@
-function number(n1) {
-    return n1
-}
+let number1 = 0
 
 document.querySelector('#digitos1 > #number1').addEventListener('click', function() {
-    number(1)
-    console.log(number(1))
+    number1 = 1
+    return number1
 })
 document.querySelector('#digitos1 > #number2').addEventListener('click', function() {
-    number(2)
+    number1 = 2
+    return number1
 })
 document.querySelector('#digitos1 > #number3').addEventListener('click', function() {
-    number(3)
+    number1 = 3
+    return number1
 })
 document.querySelector('#digitos1 > #number4').addEventListener('click', function() {
-    number(4)
+    number1 = 4
+    return number1
 })
 document.querySelector('#digitos1 > #number5').addEventListener('click', function() {
-    number(5)
+    number1 = 5
+    return number1
 })
 document.querySelector('#digitos1 > #number6').addEventListener('click', function() {
-    number(6)
+    number1 = 6
+    return number1
 })
 document.querySelector('#digitos1 > #number7').addEventListener('click', function() {
-    number(7)
+    number1 = 7
+    return number1
 })
 document.querySelector('#digitos1 > #number8').addEventListener('click', function() {
-    number(8)
+    number1 = 8
+    return number1
 })
 document.querySelector('#digitos1 > #number9').addEventListener('click', function() {    
-    number(9)
+    number1 = 9
+    return number1
 })
 document.querySelector('#digitos1 > #number0').addEventListener('click', function() {
-    number(0)
+    number1 = 0
+    return number1
 })
 
+let number2 = 0
+
+
+
+
+document.querySelector('#digitos2 > #number1').addEventListener('click', function() {
+    number2 = 1
+    return number2
+})
+document.querySelector('#digitos2 > #number2').addEventListener('click', function() {
+    number2 = 2
+    return number2
+
+})
+document.querySelector('#digitos2 > #number3').addEventListener('click', function() {
+    number2 = 3
+    return number2
+})
+document.querySelector('#digitos2 > #number4').addEventListener('click', function() {
+    number2 = 4
+    return number2
+})
+document.querySelector('#digitos2 > #number5').addEventListener('click', function() {
+    number2 = 5
+    return number2
+})
+document.querySelector('#digitos2 > #number6').addEventListener('click', function() {
+    number2 = 6
+    return number2
+})
+document.querySelector('#digitos2 > #number7').addEventListener('click', function() {
+    number2 = 7
+    return number2
+})
+document.querySelector('#digitos2 > #number8').addEventListener('click', function() {
+    number2 = 8
+    return number2
+})
+document.querySelector('#digitos2 > #number9').addEventListener('click', function() {    
+    number2 = 9
+    return number2
+})
+document.querySelector('#digitos2 > #number0').addEventListener('click', function() {
+    number2 = 0
+    return number2
+})
+
+op = "+"
+
+document.querySelector("#operadores > #multiplicar").addEventListener('click', function() {
+    op = "*"
+    return op 
+})
+
+
+
+document.querySelector("#operadores > #calcular").addEventListener('click', function() {
+    switch (op) {
+        case "*":
+            console.log(number1 * number2)
+            break;
+        case "+":
+            console.log(number1 + number2)
+            break;
+        default:
+            break;
+    }
+})
+
+document.querySelector("#operadores > #reset").addEventListener('click', function() {
+    number1 = 0
+    number2 = 0
+    op = "+"
+    return number1, number2, op  
+})
