3c1f4b0bb5aa6634355966732be35618b36e0b96
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2024-07-31 21:45:20 -0300

    coloquei mais display

--- projeto-calculadora/script.js
+++ projeto-calculadora/script.js
@@ -4,46 +4,61 @@ let number2 = 0
 let op = "+"
 let resultado = 0
 const botaoResultado = document.querySelector("#resultado") //pegando o botão de resultado para mostrar o resultado
+const displayDigito1 = document.querySelector("#digito1")
+const displayDigito2 = document.querySelector("#digito2")
+const displayOperador = document.querySelector("#operador")
+
+
 
 //o código abaixo para pegar qual é o valor do primeiro digito
 document.querySelector('#digitos1 > #number1').addEventListener('click', function() {
     number1 = 1
+    displayDigito1.innerHTML = `Digito 1: ${number1}`
     return number1
 })
 document.querySelector('#digitos1 > #number2').addEventListener('click', function() {
     number1 = 2
+    displayDigito1.innerHTML = `Digito 1: ${number1}`
     return number1
 })
 document.querySelector('#digitos1 > #number3').addEventListener('click', function() {
     number1 = 3
+    displayDigito1.innerHTML = `Digito 1: ${number1}`
     return number1
 })
 document.querySelector('#digitos1 > #number4').addEventListener('click', function() {
     number1 = 4
+    displayDigito1.innerHTML = `Digito 1: ${number1}`
     return number1
 })
 document.querySelector('#digitos1 > #number5').addEventListener('click', function() {
     number1 = 5
+    displayDigito1.innerHTML = `Digito 1: ${number1}`
     return number1
 })
 document.querySelector('#digitos1 > #number6').addEventListener('click', function() {
     number1 = 6
+    displayDigito1.innerHTML = `Digito 1: ${number1}`
     return number1
 })
 document.querySelector('#digitos1 > #number7').addEventListener('click', function() {
     number1 = 7
+    displayDigito1.innerHTML = `Digito 1: ${number1}`
     return number1
 })
 document.querySelector('#digitos1 > #number8').addEventListener('click', function() {
     number1 = 8
+    displayDigito1.innerHTML = `Digito 1: ${number1}`
     return number1
 })
 document.querySelector('#digitos1 > #number9').addEventListener('click', function() {    
     number1 = 9
+    displayDigito1.innerHTML = `Digito 1: ${number1}`
     return number1
 })
 document.querySelector('#digitos1 > #number0').addEventListener('click', function() {
     number1 = 0
+    displayDigito1.innerHTML = `Digito 1: ${number1}`
     return number1
 })
 
@@ -51,43 +66,53 @@ document.querySelector('#digitos1 > #number0').addEventListener('click', functio
 //o código abaixo serve para pegar o valor do segundo digito
 document.querySelector('#digitos2 > #number1').addEventListener('click', function() {
     number2 = 1
+    displayDigito2.innerHTML = `Digito 2: ${number2}`
     return number2
 })
 document.querySelector('#digitos2 > #number2').addEventListener('click', function() {
     number2 = 2
+    displayDigito2.innerHTML = `Digito 2: ${number2}`
     return number2
 
 })
 document.querySelector('#digitos2 > #number3').addEventListener('click', function() {
     number2 = 3
+    displayDigito2.innerHTML = `Digito 2: ${number2}`
     return number2
 })
 document.querySelector('#digitos2 > #number4').addEventListener('click', function() {
     number2 = 4
+    displayDigito2.innerHTML = `Digito 2: ${number2}`
     return number2
 })
 document.querySelector('#digitos2 > #number5').addEventListener('click', function() {
     number2 = 5
+    displayDigito2.innerHTML = `Digito 2: ${number2}`
     return number2
 })
 document.querySelector('#digitos2 > #number6').addEventListener('click', function() {
     number2 = 6
+    displayDigito2.innerHTML = `Digito 2: ${number2}`
     return number2
 })
 document.querySelector('#digitos2 > #number7').addEventListener('click', function() {
     number2 = 7
+    displayDigito2.innerHTML = `Digito 2: ${number2}`
     return number2
 })
 document.querySelector('#digitos2 > #number8').addEventListener('click', function() {
     number2 = 8
+    displayDigito2.innerHTML = `Digito 2: ${number2}`
     return number2
 })
 document.querySelector('#digitos2 > #number9').addEventListener('click', function() {    
     number2 = 9
+    displayDigito2.innerHTML = `Digito 2: ${number2}`
     return number2
 })
 document.querySelector('#digitos2 > #number0').addEventListener('click', function() {
     number2 = 0
+    displayDigito2.innerHTML = `Digito 2: ${number2}`
     return number2
 })
 
@@ -95,18 +120,22 @@ document.querySelector('#digitos2 > #number0').addEventListener('click', functio
 //o código abaixo serve para pegar qual é o operador
 document.querySelector("#operadores > #multiplicar").addEventListener('click', function() {
     op = "*"
+    displayOperador.innerHTML = `Operador: ${op}`
     return op 
 })
 document.querySelector("#operadores > #div").addEventListener('click', function() {
     op = "/"
+    displayOperador.innerHTML = `Operador: ${op}`
     return op 
 })
 document.querySelector("#operadores > #minus").addEventListener('click', function() {
     op = "-"
+    displayOperador.innerHTML = `Operador: ${op}`
     return op 
 })
 document.querySelector("#operadores > #plus").addEventListener('click', function() {
     op = "+"
+    displayOperador.innerHTML = `Operador: ${op}`
     return op 
 })
 
@@ -115,6 +144,9 @@ function reset() { //função para resetar os valores após a operação
     number1 = 0
     number2 = 0
     op = "+"
+    displayDigito1.innerHTML = `Digito 1: 0`
+    displayDigito2.innerHTML = `Digito 2: 0`
+    displayOperador.innerHTML = `Operador: +`
     return number1, number2, op
 }
 
