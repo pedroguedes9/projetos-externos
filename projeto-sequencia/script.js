a23f8030900c477907dce474acd91d0fe041e99b
pedroguedes9 <160842810+pedroguedes9@users.noreply.github.com>
2024-12-30 14:18:23 -0300

    Finalizamos a parte princcipal

--- projeto-sequencia/script.js
+++ projeto-sequencia/script.js
@@ -4,11 +4,14 @@ let mainSequence = []
 let sequenceNumber = []
 let correctNumbers = 0
 let correctPositions = 0
+let times = 0
 generateSequence()
 
 
 //events
 document.querySelector("#sequence-form").addEventListener("submit", event => {
+    
+    times++
     event.preventDefault();
     correctNumbers = 0
     correctPositions = 0
@@ -19,8 +22,17 @@ document.querySelector("#sequence-form").addEventListener("submit", event => {
     
     historySequence.id = "history-sequence";
     resultSequence.id = "result-sequence";
-    document.querySelector("#history").appendChild(historySequence);
-    document.querySelector("#history").appendChild(resultSequence);
+    let historyContainer = document.createElement("div");
+    document.querySelector("#history").prepend(historyContainer);
+
+    historyContainer.appendChild(historySequence);
+    historyContainer.appendChild(resultSequence);
+    historySequence.innerHTML +=
+    `
+    <p class="history-title"> ${times}- </p>
+    `
+
+
     for(let i=0; i<3;i++){
         historySequence.innerHTML += 
     `
@@ -57,24 +69,29 @@ function generateSequence() {
     
 }
 function checkSequence() {
+
     correctNumbers = 0
     correctPositions = 0
+    let numberUsed = []
+    numberUsed = [false,false,false]
+    
+    
     for(let i = 0; i <3; i++) {
         for(let j = 0; j <3; j++) {
-            if(mainSequence[j] == sequenceNumber[i]) {
-                correctNumbers++
-                break
+            if(numberUsed[j] == false){
+                if(mainSequence[i] == sequenceNumber[j]) {
+                correctNumbers++;
+                numberUsed[j] = true
+                break;
+                }
             }
             
         }
     }
     for(let i = 0; i <3; i++) {
-        for(let j = 0; j <3; j++) {
-
-            if(sequenceNumber[i] == mainSequence[j] && i == j) {
+            if(sequenceNumber[i] == mainSequence[i]) {
                 correctPositions++
             }
-        }
     }
 }
 
