cdd498c04ffd176ceb36859b411c342a6e4605b0
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2025-01-07 13:51:16 -0300

    .

--- projeto-binario/script.js
+++ projeto-binario/script.js
@@ -2,39 +2,77 @@
 const inputElement = document.querySelector("#inputValue")
 const resultElement = document.querySelector("#result")
 
+
+let bin = []
+let result = []
+
 //events
 document.querySelector("#formRadio").addEventListener("change", () => {
     inputElement.value = ""
+    if(inputElement.placeholder === "Digite o valor Binário") {
+        inputElement.placeholder = "Digite o valor Decimal"
+    }
+    else{
+        inputElement.placeholder = "Digite o valor Binário"
+        
+    }
+    
+
 })
 document.querySelector("#convertButton").addEventListener("click", event => { 
     const value = inputElement.value
-    const conversion = document.querySelector("input[name='opChoice']:checked").value
+    let conversion = document.querySelector("input[name='opChoice']:checked").value
     conversionChoice(conversion, value)
+    
+})
+document.querySelector("#copyButton").addEventListener("click", event => {
+    event.preventDefault()
+    const resultText = resultElement.value
+    if(resultText.trim() === "") {
+        alert("Nada para copiar")
+        return
+    }
+    else {
+        navigator.clipboard.writeText(resultText)
+            .then(() => {
+                alert("Texto copiado com sucesso!")
+            })
+            .catch(err => {
+                console.error("Erro ao copiar o texto: ", err);
+            })
+        
+    }
 })
 
 //functions
-function conversionChoice(choice, value) {
+function conversionChoice(choice, valueString) {
+    console.log(valueString)
+    let valueTrim = valueString.trim()
+    console.log(valueTrim)
+    let valueNumber = Number(valueTrim)
     if(choice == "decToBin") {
-        decToBin(value)
+        result = decToBin(valueNumber)
+        
     }
     else{
-        binToDec(value)
+        result = binToDec(valueNumber)
+        
     }
-    resultElement.textContent = `Resultado: ${bin.join("")}`
+    resultElement.textContent = `${result.join("")}`;
 }
+
 
 function decToBin(value) {
     let bin = [];
-    
     while(value > 0){
         bin.unshift(value % 2);
         value = Math.floor(value / 2); 
     }
-
+    return bin
     
 }
 
 function binToDec(value){
-    console.log(value.split())
+    
 }
 
