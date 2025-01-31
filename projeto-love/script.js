08588a14100c8dc4b52af990c8680de57de7514d
Pedro Guedes <160842810+pedroguedes9@users.noreply.github.com>
2025-01-31 18:19:32 -0300

    .

--- projeto-love/script.js
+++ projeto-love/script.js
@@ -2,31 +2,55 @@ const dataPedido = new Date(2025, 0, 4, 15, 0, 0)
 let agora, tempoDecorrido, anos, meses, dias, horas, minutos, segundos
 let display = Array.from(document.querySelectorAll(".display"))
 let tempo = []
+let recado = document.querySelector("#recado")
 console.log(display)
 console.log(dataPedido)
-
-
+document.querySelector("#clique").addEventListener("click", clique => {
+    if(getComputedStyle(recado).display === "none") {
+        recado.style.display = "block"
+    } else {
+        recado.style.display = "none"
+    }
+})
 function atualizar() {
     agora = new Date()
     tempoDecorrido = agora - dataPedido
     console.log(tempoDecorrido)
+    console.log(agora, agora.getDate(), dataPedido.getDate())
 
-    anos = Math.floor(tempoDecorrido / (1000 * 60 * 60 * 24 * 365))
-    meses = Math.floor(tempoDecorrido / (1000 * 60 * 60 * 24 * (365/12)))
-    dias = Math.floor(tempoDecorrido / (1000 * 60 * 60 * 24))
-    horas = Math.floor((tempoDecorrido / (1000 * 60 * 60) )% 24)
-    minutos = Math.floor((tempoDecorrido / (1000 * 60)) % 60)
-    segundos = Math.floor((tempoDecorrido / 1000) % 60)
+    anos = agora.getFullYear() - dataPedido.getFullYear()
+    meses = agora.getMonth() - dataPedido.getMonth()
+    dias = agora.getDate() - dataPedido.getDate()
+    horas = agora.getHours() - dataPedido.getHours()
+    minutos = agora.getMinutes() - dataPedido.getMinutes()
+    segundos = agora.getSeconds() - dataPedido.getSeconds()
     
-    
+    if (segundos < 0) {
+        segundos += 60;
+        minutos--;
+    }
+    if (minutos < 0) {
+        minutos += 60;
+        horas--;
+    }
+    if (horas < 0) {
+        horas += 24;
+        dias--;
+    }
+    if (dias < 0) {
+        let ultimoDiaMesPassado = new Date(agora.getFullYear(), agora.getMonth(), 0).getDate();
+        dias += ultimoDiaMesPassado;
+        meses--;
+    }
+    if (meses < 0) {
+        meses += 12;
+        anos--;
+    }
 
     tempo = [anos, meses, dias, horas, minutos, segundos]
-    for (let i = 0; i < display.length; i++) {
-        display[i].textContent = tempo[i]
-        
-        
-    }
-    
+    tempo.forEach((valor, i) => {
+        display[i].textContent = valor
+    })
 }
 let intervalo = setInterval(atualizar, 1000)
 atualizar()
